c823fa4fcf55e48c12d04521d76db51bf0a83a1d
Kostas Karayannis <kkarayannis@gmail.com>
2015-02-09 11:12:23 +0000

    Fixed Done button in address selection screen. Removed API key

--- PSPrintSDK/OLScrollCropViewController.m
+++ PSPrintSDK/OLScrollCropViewController.m
@@ -22,7 +22,7 @@
     
     UIButton *doneButton = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 40, 20)];
     [doneButton addTarget:self action:@selector(onBarButtonDoneTapped:) forControlEvents:UIControlEventTouchUpInside];
-    [doneButton setTitle: @"Done" forState:UIControlStateNormal];
+    [doneButton setTitle:NSLocalizedString(@"Done", @"") forState:UIControlStateNormal];
     [doneButton setTitleColor:self.view.tintColor forState:UIControlStateNormal];
     [doneButton.titleLabel setFont:[UIFont boldSystemFontOfSize:18]];
     [doneButton sizeToFit];
