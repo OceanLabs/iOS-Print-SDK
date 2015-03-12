4165bf25bda9b550549220b6143a1205c75b35cb
Kostas Karayannis <kkarayannis@gmail.com>
2015-03-12 17:19:04 +0000

    Rotate image after crop

--- Kite SDK/PSPrintSDK/RMImageCropper/RMImageCropper.m
+++ Kite SDK/PSPrintSDK/RMImageCropper/RMImageCropper.m
@@ -284,7 +284,7 @@ CGFloat const RESET_DURATION = 0.10f;
     image = [RMImageCropper imageByRotatingImage:image fromImageOrientation:image.imageOrientation];
     
     CGImageRef imageReference = CGImageCreateWithImageInRect([image CGImage], imageCroppingRect);
-    UIImage *croppedImage = [UIImage imageWithCGImage:imageReference scale:image.scale orientation:image.imageOrientation];
+    UIImage *croppedImage = [UIImage imageWithCGImage:imageReference scale:image.scale orientation:UIImageOrientationUp]; //Hardcoded orientation metadata because we have rotated the actual image.
     CGImageRelease(imageReference);
     
     return croppedImage;
@@ -292,119 +292,12 @@ CGFloat const RESET_DURATION = 0.10f;
 
 +(UIImage*)imageByRotatingImage:(UIImage*)initImage fromImageOrientation:(UIImageOrientation)orientation
 {
-    CGImageRef imgRef = initImage.CGImage;
-    
-    CGFloat width = CGImageGetWidth(imgRef);
-    CGFloat height = CGImageGetHeight(imgRef);
-    
-    CGAffineTransform transform = CGAffineTransformIdentity;
-    CGRect bounds = CGRectMake(0, 0, width, height);
-    CGSize imageSize = CGSizeMake(CGImageGetWidth(imgRef), CGImageGetHeight(imgRef));
-    CGFloat boundHeight;
-    UIImageOrientation orient = orientation;
-    switch(orient) {
-            
-        case UIImageOrientationUp: //EXIF = 1
-            return initImage;
-            break;
-            
-        case UIImageOrientationUpMirrored: //EXIF = 2
-            transform = CGAffineTransformMakeTranslation(imageSize.width, 0.0);
-            transform = CGAffineTransformScale(transform, -1.0, 1.0);
-            break;
-            
-        case UIImageOrientationDown: //EXIF = 3
-            transform = CGAffineTransformMakeTranslation(imageSize.width, imageSize.height);
-            transform = CGAffineTransformRotate(transform, M_PI);
-            break;
-            
-        case UIImageOrientationDownMirrored: //EXIF = 4
-            transform = CGAffineTransformMakeTranslation(0.0, imageSize.height);
-            transform = CGAffineTransformScale(transform, 1.0, -1.0);
-            break;
-            
-        case UIImageOrientationLeftMirrored: //EXIF = 5
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeTranslation(imageSize.height, imageSize.width);
-            transform = CGAffineTransformScale(transform, -1.0, 1.0);
-            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
-            break;
-            
-        case UIImageOrientationLeft: //EXIF = 6
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeTranslation(0.0, imageSize.width);
-            transform = CGAffineTransformRotate(transform, 3.0 * M_PI / 2.0);
-            break;
-            
-        case UIImageOrientationRightMirrored: //EXIF = 7
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeScale(-1.0, 1.0);
-            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
-            break;
-            
-        case UIImageOrientationRight: //EXIF = 8
-            boundHeight = bounds.size.height;
-            bounds.size.height = bounds.size.width;
-            bounds.size.width = boundHeight;
-            transform = CGAffineTransformMakeTranslation(imageSize.height, 0.0);
-            transform = CGAffineTransformRotate(transform, M_PI / 2.0);
-            break;
-            
-        default:
-            [NSException raise:NSInternalInconsistencyException format:@"Invalid image orientation"];
-            
-    }
-    // Create the bitmap context
-    CGContextRef    context = NULL;
-    void *          bitmapData;
-    int             bitmapByteCount;
-    int             bitmapBytesPerRow;
-    
-    // Declare the number of bytes per row. Each pixel in the bitmap in this
-    // example is represented by 4 bytes; 8 bits each of red, green, blue, and
-    // alpha.
-    bitmapBytesPerRow   = (bounds.size.width * 4);
-    bitmapByteCount     = (bitmapBytesPerRow * bounds.size.height);
-    bitmapData = malloc( bitmapByteCount );
-    if (bitmapData == NULL)
-    {
-        return nil;
-    }
-    
-    // Create the bitmap context. We want pre-multiplied ARGB, 8-bits
-    // per component. Regardless of what the source image format is
-    // (CMYK, Grayscale, and so on) it will be converted over to the format
-    // specified here by CGBitmapContextCreate.
-    CGColorSpaceRef colorspace = CGImageGetColorSpace(imgRef);
-    context = CGBitmapContextCreate (bitmapData,bounds.size.width,bounds.size.height,8,bitmapBytesPerRow,
-                                     colorspace, kCGBitmapAlphaInfoMask & kCGImageAlphaPremultipliedLast);
-    
-    if (context == NULL)
-        // error creating context
-        return nil;
-    
-    CGContextScaleCTM(context, -1.0, -1.0);
-    CGContextTranslateCTM(context, -bounds.size.width, -bounds.size.height);
-    
-    CGContextConcatCTM(context, transform);
-    
-    // Draw the image to the bitmap context. Once we draw, the memory
-    // allocated for the context for rendering will then contain the
-    // raw image data in the specified color space.
-    CGContextDrawImage(context, CGRectMake(0,0,width, height), imgRef);
-    
-    CGImageRef imgRef2 = CGBitmapContextCreateImage(context);
-    CGContextRelease(context);
-    free(bitmapData);
-    UIImage * image = [UIImage imageWithCGImage:imgRef2 scale:initImage.scale orientation:UIImageOrientationUp];
-    CGImageRelease(imgRef2);
-    return image;
+    CGSize size = initImage.size;
+    UIGraphicsBeginImageContext(CGSizeMake(size.height, size.width));
+    [[UIImage imageWithCGImage:[initImage CGImage] scale:1.0 orientation:orientation] drawInRect:CGRectMake(0,0,size.width ,size.height)];
+    UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
 }
 
 - (void)setMinimum:(NSInteger)minimum
