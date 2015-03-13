33675c36cd31849531729b73e6c7ce4c4c5b0a81
Kostas Karayannis <kkarayannis@gmail.com>
2015-03-13 10:43:03 +0000

    Fixed glitches on iOS 7 and iPhone 4

--- Kite SDK/PSPrintSDK/OLKiteViewController.m
+++ Kite SDK/PSPrintSDK/OLKiteViewController.m
@@ -69,7 +69,7 @@
     }];
 }
 
--(void) viewWillAppear:(BOOL)animated{
+-(void) viewDidAppear:(BOOL)animated{
     if (self.presentLater){
         self.presentLater = NO;
         [self presentNextVc];
