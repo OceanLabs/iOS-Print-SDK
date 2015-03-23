d31cf062d31b86d9a4d0501952b7d83004df2490
Deon Botha <deon@dbotha.com>
2015-03-23 18:10:12 +0000

    Removed erroneous API key presence

--- Kite SDK/PSPrintSDK/ViewController.m
+++ Kite SDK/PSPrintSDK/ViewController.m
@@ -15,7 +15,7 @@
  * Insert your API keys here. These are found under your profile 
  * by logging in to the developer portal at http://kite.ly
  **********************************************************************/
-static NSString *const kAPIKeySandbox = @"ba171b0d91b1418fbd04f7b12af1e37e42d2cb1e"; // replace with your Sandbox API key found under the Profile section in the developer portal
+static NSString *const kAPIKeySandbox = @"REPLACE_WITH_YOUR_API_KEY"; // replace with your Sandbox API key found under the Profile section in the developer portal
 static NSString *const kAPIKeyLive = @"REPLACE_WITH_YOUR_API_KEY"; // replace with your Live API key found under the Profile section in the developer portal
 
 static NSString *const kStripePublishableKey = @"pk_test_6pRNASCoBOKtIshFeQd4XMUh"; // This is a test key. Replace with the live key here.
