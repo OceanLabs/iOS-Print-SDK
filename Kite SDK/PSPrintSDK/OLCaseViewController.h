3842c5f71b050a54d340899979326d7c4c77c540
Kostas Karayannis <kkarayannis@gmail.com>
2015-04-07 16:19:41 +0100

    Subclassed OLSingleImageViewController with OLCaseViewController for cases.

---
+++ Kite SDK/PSPrintSDK/OLCaseViewController.h
@@ -0,0 +1,13 @@
+//
+//  OLCaseViewController.h
+//  KitePrintSDK
+//
+//  Created by Konstadinos Karayannis on 4/7/15.
+//  Copyright (c) 2015 Deon Botha. All rights reserved.
+//
+
+#import "OLSingleImageProductReviewViewController.h"
+
+@interface OLCaseViewController : OLSingleImageProductReviewViewController
+
+@end
--- Kite SDK/PSPrintSDK/OLSingleImageProductReviewViewController.m
+++ Kite SDK/PSPrintSDK/OLSingleImageProductReviewViewController.m
@@ -11,7 +11,6 @@
 #import "OLAnalytics.h"
 #import "OLAsset+Private.h"
 #import <SDWebImageManager.h>
-#import "RMImageCropper.h"
 #import "OLProductPrintJob.h"
 #import "OLKitePrintSDK.h"
 #import <CTAssetsPickerController.h>
@@ -49,18 +48,16 @@ OLFacebookImagePickerControllerDelegate,
 #endif
 CTAssetsPickerControllerDelegate>
 
-@property (weak, nonatomic) IBOutlet RMImageCropper *imageCropView;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *maskAspectRatio;
-@property (weak, nonatomic) IBOutlet UIView *containerView;
 @property (weak, nonatomic) IBOutlet UILabel *quantityLabel;
 @property (assign, nonatomic) NSUInteger quantity;
-@property (assign, nonatomic) BOOL downloadedMask;
 
-@property (strong, nonatomic) UIImage *maskImage;
-@property (strong, nonatomic) UIVisualEffectView *visualEffectView;
-@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *maskActivityIndicator;
 @property (weak, nonatomic) IBOutlet UICollectionView *imagesCollectionView;
 
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet RMImageCropper *imageCropView;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *maskAspectRatio;
+
+-(void) doCheckout;
 
 @end
 
@@ -68,7 +65,6 @@ CTAssetsPickerControllerDelegate>
 
 -(void)viewDidLoad{
     [super viewDidLoad];
-    self.downloadedMask = NO;
     
 #ifndef OL_NO_ANALYTICS
     [OLAnalytics trackReviewScreenViewed:self.product.productTemplate.name];
@@ -103,93 +99,8 @@ CTAssetsPickerControllerDelegate>
 
 -(void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
-    
-    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0){
-        if (!self.visualEffectView){
-            UIVisualEffect *blurEffect;
-            blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleLight];
-            
-            self.visualEffectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
-            UIView *view = self.visualEffectView;
-            [view.layer setMasksToBounds:YES];
-            [view.layer setCornerRadius:45.0f];
-            [self.containerView insertSubview:view belowSubview:self.maskActivityIndicator];
-            
-            view.translatesAutoresizingMaskIntoConstraints = NO;
-            NSDictionary *views = NSDictionaryOfVariableBindings(view);
-            NSMutableArray *con = [[NSMutableArray alloc] init];
-            
-            NSArray *visuals = @[@"H:|-0-[view]-0-|",
-                                 @"V:|-0-[view]-0-|"];
-            
-            
-            for (NSString *visual in visuals) {
-                [con addObjectsFromArray: [NSLayoutConstraint constraintsWithVisualFormat:visual options:0 metrics:nil views:views]];
-            }
-            
-            [view.superview addConstraints:con];
-        }
-    }
-    else{
-        
-    }
-    
-    UIImage *tempMask = [UIImage imageNamed:@"dummy mask"];
-    [self.containerView removeConstraint:self.maskAspectRatio];
-    NSLayoutConstraint *con = [NSLayoutConstraint constraintWithItem:self.containerView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.containerView attribute:NSLayoutAttributeWidth multiplier:tempMask.size.height / tempMask.size.width constant:0];
-    [self.containerView addConstraints:@[con]];
-    self.maskAspectRatio = con;
-    
-    [self.view setNeedsLayout];
-    [self.view layoutIfNeeded];
-    
-    [self maskWithImage:tempMask targetView:self.imageCropView];
-    
-    [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
-    [self downloadMask];
 }
 
-- (void)downloadMask {
-    [[SDWebImageManager sharedManager] downloadImageWithURL:self.product.productTemplate.maskImageURL options:SDWebImageHighPriority progress:NULL completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL){
-        [[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
-        if (error) {
-            UIAlertView *av = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Oops", @"")  message:NSLocalizedString(@"Failed to download phone case mask. Please check your internet connectivity and try again", @"")  delegate:self cancelButtonTitle:NSLocalizedString(@"Cancel", @"") otherButtonTitles:@"Retry", nil];
-            [av show];
-        } else {
-            [self.containerView removeConstraint:self.maskAspectRatio];
-            NSLayoutConstraint *con = [NSLayoutConstraint constraintWithItem:self.containerView attribute:NSLayoutAttributeHeight relatedBy:NSLayoutRelationEqual toItem:self.containerView attribute:NSLayoutAttributeWidth multiplier:self.product.productTemplate.sizePx.height / self.product.productTemplate.sizePx.width constant:0];
-            [self.containerView addConstraints:@[con]];
-            
-            [self.view setNeedsLayout];
-            [self.view layoutIfNeeded];
-            
-            [self maskWithImage:image targetView:self.imageCropView];
-            self.visualEffectView.hidden = YES;
-            self.downloadedMask = YES;
-            [self.maskActivityIndicator removeFromSuperview];
-            self.maskActivityIndicator = nil;
-        }
-    }];
-}
-
--(void) maskWithImage:(UIImage*) maskImage targetView:(UIView*) targetView{
-    CALayer *_maskingLayer = [CALayer layer];
-    CGRect f = targetView.bounds;
-    UIEdgeInsets imageBleed = self.product.productTemplate.imageBleed;
-    CGSize size = self.product.productTemplate.sizePx;
-    
-    UIEdgeInsets adjustedBleed = UIEdgeInsetsMake(f.size.height * imageBleed.top / size.height,
-                                                  f.size.width * imageBleed.left / size.width,
-                                                  f.size.height * imageBleed.bottom / size.height,
-                                                  f.size.width * imageBleed.right / size.width);
-    
-    _maskingLayer.frame = CGRectMake(f.origin.x + adjustedBleed.left,
-                                     f.origin.y + adjustedBleed.top,
-                                     f.size.width - (adjustedBleed.left + adjustedBleed.right),
-                                     f.size.height - (adjustedBleed.top + adjustedBleed.bottom));
-    [_maskingLayer setContents:(id)[maskImage CGImage]];
-    [targetView.layer setMask:_maskingLayer];
-}
 
 -(NSMutableArray *) userSelectedPhotos{
     if (!_userSelectedPhotos){
@@ -225,7 +136,7 @@ CTAssetsPickerControllerDelegate>
 }
 
 -(void) doCheckout{
-    if (!self.imageCropView.image || !self.downloadedMask) {
+    if (!self.imageCropView.image) {
         return;
     }
     
@@ -601,16 +512,6 @@ CTAssetsPickerControllerDelegate>
 
 - (NSUInteger)supportedInterfaceOrientations {
     return UIInterfaceOrientationMaskPortrait;
-}
-
-#pragma mark - UIAlertViewDelegate methods
-
-- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex {
-    if (buttonIndex == 0) {
-        [self.navigationController popViewControllerAnimated:YES];
-    } else {
-        [self downloadMask];
-    }
 }
 
 @end
