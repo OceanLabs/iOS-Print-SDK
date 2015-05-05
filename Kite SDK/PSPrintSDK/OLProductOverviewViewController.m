0ed7acfd5975bbb23dbc00903e2dcea1026fabd5
Kostas Karayannis <kkarayannis@gmail.com>
2015-05-05 11:25:45 +0100

    Fixed transitioning to the photo book screen when photo selection is disabled

--- Kite SDK/PSPrintSDK/OLProductOverviewViewController.m
+++ Kite SDK/PSPrintSDK/OLProductOverviewViewController.m
@@ -122,6 +122,9 @@
     else if (self.product.productTemplate.templateUI == kOLTemplateUIPoster){
         vc = [self.storyboard instantiateViewControllerWithIdentifier:@"OLSingleImageProductReviewViewController"];
     }
+    else if (self.product.productTemplate.templateUI == kOLTemplateUIPhotobook){
+        vc = [self.storyboard instantiateViewControllerWithIdentifier:@"PhotobookViewController"];
+    }
     else{
         if (![self.delegate respondsToSelector:@selector(kiteControllerShouldAllowUserToAddMorePhotos:)] || [self.delegate kiteControllerShouldAllowUserToAddMorePhotos:[OLKitePrintSDK kiteViewControllerInNavStack:self.navigationController.viewControllers]]){
             vc = [self.storyboard instantiateViewControllerWithIdentifier:@"PhotoSelectionViewController"];
