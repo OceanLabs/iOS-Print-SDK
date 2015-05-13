90257bc7afa640905386404d6940d6c7fa55f14c
Kostas Karayannis <kkarayannis@gmail.com>
2015-05-13 12:00:21 +0300

    Added rounding behavior so that we don't have more than 2 decimal digits in the price

--- Kite SDK/PSPrintSDK/OLPaymentLineItemPercentage.m
+++ Kite SDK/PSPrintSDK/OLPaymentLineItemPercentage.m
@@ -10,11 +10,41 @@
 
 static NSString *const kKeyLineItemJob = @"co.oceanlabs.kKeyLineItemJob";
 
+@interface OLDecimalNumberBehavior : NSObject <NSDecimalNumberBehaviors>
+
+@end
+
+@implementation OLDecimalNumberBehavior
+
+- (NSRoundingMode)roundingMode{
+    return NSRoundPlain;
+}
+
+- (short)scale{
+    return 2;
+}
+
+- (NSDecimalNumber *)exceptionDuringOperation:(SEL)operation error:(NSCalculationError)error leftOperand:(NSDecimalNumber *)leftOperand rightOperand:(NSDecimalNumber *)rightOperand{
+    if (error != NSCalculationDivideByZero){
+        return nil;
+    }
+    else{
+        NSException* exception = [NSException
+                                    exceptionWithName:@"DivideByZeroException"
+                                    reason:@"Attempted to divide a number by zero."
+                                    userInfo:nil];
+        @throw exception;
+    }
+}
+
+@end
+
 @implementation OLPaymentLineItemPercentage
 
 - (NSDecimalNumber *)price{
     if (self.printJob && self.currencyCode){
-        return [self.value decimalNumberByMultiplyingBy:[self.printJob costInCurrency:self.currencyCode]];
+        NSDecimalNumber *decimal = [self.value decimalNumberByMultiplyingBy:[self.printJob costInCurrency:self.currencyCode]];
+        return [decimal decimalNumberByRoundingAccordingToBehavior:[[OLDecimalNumberBehavior alloc] init]];
     }
     else{
         return [super price];
