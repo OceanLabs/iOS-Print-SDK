6ea8ca6f7f43998cc8a25c194e725d67b75a4f6f
Deon Botha <deon@dbotha.com>
2015-05-25 16:39:36 +0100

    Bumped API endpoint version

--- Kite SDK/PSPrintSDK/OLKitePrintSDK.m
+++ Kite SDK/PSPrintSDK/OLKitePrintSDK.m
@@ -36,7 +36,7 @@ static NSString *const kOLPayPalClientIdLive = @"ASYVBBCHF_KwVUstugKy4qvpQaPlUeE
 static NSString *const kOLPayPalClientIdSandbox = @"AcEcBRDxqcCKiikjm05FyD4Sfi4pkNP98AYN67sr3_yZdBe23xEk0qhdhZLM";
 static NSString *const kOLPayPalRecipientEmailLive = @"hello@kite.ly";
 static NSString *const kOLPayPalRecipientEmailSandbox = @"sandbox-merchant@kite.ly";
-static NSString *const kOLAPIEndpointVersion = @"v1.3";
+static NSString *const kOLAPIEndpointVersion = @"v1.4";
 
 static BOOL useJudoPayForGBP = NO;
 static BOOL cacheTemplates = NO;
