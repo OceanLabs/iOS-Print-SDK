a7c331b49e1271acbe717d2a5c86bba60b9c6c33
Kostas Karayannis <kkarayannis@gmail.com>
2015-08-25 12:23:34 +0300

    Moved string generation to its own method.

--- Kite SDK/PSPrintSDK/OLProductOverviewViewController.m
+++ Kite SDK/PSPrintSDK/OLProductOverviewViewController.m
@@ -115,46 +115,7 @@
         }
     }
     
-    NSString *s = @"";
-    
-    //Add description
-    if (self.product.productTemplate.productDescription && ![self.product.productTemplate.productDescription isEqualToString:@""]){
-        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Description**\n%@\n\n", self.product.productTemplate.productDescription]];
-    }
-    
-    //Add size info
-    OLTemplateUI templateClass = self.product.productTemplate.templateUI;
-    if (templateClass != kOLTemplateUICase){
-        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Size**\n%@\n\n", self.product.dimensions]];
-    }
-    
-    //Add qty info
-    if (self.product.packInfo && ![self.product.packInfo isEqualToString:@""]){
-        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Quantity**\n%lu\n\n", (unsigned long)self.product.quantityToFulfillOrder]];
-    }
-    
-    //Add price info
-    if ([OLKiteABTesting sharedInstance].hidePrice){
-        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Price**\n%@\n\n", self.product.unitCost]];
-    }
-    
-    //Add shipping info
-    NSDecimalNumber *shippingCost = [self.product.productTemplate shippingCostForCountry:[OLCountry countryForCurrentLocale]];
-    if (shippingCost && [shippingCost doubleValue] != 0){
-        if (![OLKiteABTesting sharedInstance].hidePrice){
-            s = [s stringByAppendingString: [NSString stringWithFormat:NSLocalizedString(@"**Shipping**\n%@\n\n", @""), [shippingCost formatCostForCurrencyCode:[self.product.productTemplate currencyForCurrentLocale]]]];
-        }
-    }
-    else if (!shippingCost){ // ¯\_(ツ)_/¯ don't assume 0, don't add any shipping info
-    }
-    else{
-        s = [s stringByAppendingString:NSLocalizedString(@"**Shipping**\nFREE\n\n", @"")];
-    }
-    
-    //Add quality guarantee
-    s = [s stringByAppendingString:NSLocalizedString(@"**Quality Guarantee**\nWe are confident that you will be more than happy with our products. But just incase, we offer a full refund if you are not 100% happy - Even if it’s just a bad day!", @"")];
-    
-    NSMutableAttributedString *attributedString = [[[TSMarkdownParser standardParser] attributedStringFromMarkdown:s] mutableCopy];
+    NSMutableAttributedString *attributedString = [[[TSMarkdownParser standardParser] attributedStringFromMarkdown:[self stringForDetailsBox]] mutableCopy];
     
     [attributedString addAttribute:NSForegroundColorAttributeName value:[UIColor colorWithRed: 0.341 green: 0.341 blue: 0.341 alpha: 1] range:NSMakeRange(0, attributedString.length)];
     self.detailsTextLabel.attributedText = attributedString;
@@ -201,6 +162,48 @@
         self.detailsView.backgroundColor = [UIColor whiteColor];
     }
     
+}
+
+- (NSString *)stringForDetailsBox{
+    NSString *s = @"";
+    
+    //Add description
+    if (self.product.productTemplate.productDescription && ![self.product.productTemplate.productDescription isEqualToString:@""]){
+        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Description**\n%@\n\n", self.product.productTemplate.productDescription]];
+    }
+    
+    //Add size info
+    OLTemplateUI templateClass = self.product.productTemplate.templateUI;
+    if (templateClass != kOLTemplateUICase){
+        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Size**\n%@\n\n", self.product.dimensions]];
+    }
+    
+    //Add qty info
+    if (self.product.packInfo && ![self.product.packInfo isEqualToString:@""]){
+        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Quantity**\n%lu\n\n", (unsigned long)self.product.quantityToFulfillOrder]];
+    }
+    
+    //Add price info
+    if ([OLKiteABTesting sharedInstance].hidePrice){
+        s = [s stringByAppendingString:[NSString stringWithFormat:@"**Price**\n%@\n\n", self.product.unitCost]];
+    }
+    
+    //Add shipping info
+    NSDecimalNumber *shippingCost = [self.product.productTemplate shippingCostForCountry:[OLCountry countryForCurrentLocale]];
+    if (shippingCost && [shippingCost doubleValue] != 0){
+        if (![OLKiteABTesting sharedInstance].hidePrice){
+            s = [s stringByAppendingString: [NSString stringWithFormat:NSLocalizedString(@"**Shipping**\n%@\n\n", @""), [shippingCost formatCostForCurrencyCode:[self.product.productTemplate currencyForCurrentLocale]]]];
+        }
+    }
+    else if (!shippingCost){ // ¯\_(ツ)_/¯ don't assume 0, don't add any shipping info
+    }
+    else{
+        s = [s stringByAppendingString:NSLocalizedString(@"**Shipping**\nFREE\n\n", @"")];
+    }
+    
+    //Add quality guarantee
+    s = [s stringByAppendingString:NSLocalizedString(@"**Quality Guarantee**\nWe are confident that you will be more than happy with our products. But just incase, we offer a full refund if you are not 100% happy - Even if it’s just a bad day!", @"")];
+    return s;
 }
 
 - (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator{
