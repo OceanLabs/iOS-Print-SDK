9b890f2a27fe5cc41d34837b088b475895f7f345
Kostas Karayannis <kkarayannis@gmail.com>
2016-01-08 10:12:50 +0200

    Fix merged file

--- Kite-SDK/PSPrintSDK/ViewController.m
+++ Kite-SDK/PSPrintSDK/ViewController.m
@@ -1,10 +1,41 @@
 //
-//  ViewController.m
-//  Kite SDK
+//  Modified MIT License
 //
-//  Created by Deon Botha on 18/12/2013.
-//  Copyright (c) 2013 Deon Botha. All rights reserved.
+//  Copyright (c) 2010-2015 Kite Tech Ltd. https://www.kite.ly
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The software MAY ONLY be used with the Kite Tech Ltd platform and MAY NOT be modified
+//  to be used with any competitor platforms. This means the software MAY NOT be modified
+//  to place orders with any competitors to Kite Tech Ltd, all orders MUST go through the
+//  Kite Tech Ltd platform servers.
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+//
+
+/**********************************************************************
+ * Insert your API keys here. These are found under your profile
+ * by logging in to the developer portal at https://www.kite.ly
+ **********************************************************************/
+static NSString *const kAPIKeySandbox = @"REPLACE_WITH_YOUR_API_KEY"; // replace with your Sandbox API key found under the Profile section in the developer portal
+static NSString *const kAPIKeyLive = @"REPLACE_WITH_YOUR_API_KEY"; // replace with your Live API key found under the Profile section in the developer portal
+
+static NSString *const kApplePayMerchantIDKey = @"merchant.ly.kite.sdk"; // Replace with your merchant ID
+static NSString *const kApplePayBusinessName = @"Kite.ly"; //Replace with your business name
 
 #import "ViewController.h"
 #import "OLKitePrintSDK.h"
@@ -17,16 +48,6 @@
 
 #import <AssetsLibrary/AssetsLibrary.h>
 @import Photos;
-
-/**********************************************************************
- * Insert your API keys here. These are found under your profile 
- * by logging in to the developer portal at https://www.kite.ly
- **********************************************************************/
-static NSString *const kAPIKeySandbox = @"REPLACE_WITH_YOUR_API_KEY"; // replace with your Sandbox API key found under the Profile section in the developer portal
-static NSString *const kAPIKeyLive = @"REPLACE_WITH_YOUR_API_KEY"; // replace with your Live API key found under the Profile section in the developer portal
-
-static NSString *const kApplePayMerchantIDKey = @"merchant.ly.kite.sdk"; // Replace with your merchant ID
-static NSString *const kApplePayBusinessName = @"Kite.ly"; //Replace with your business name
 
 @interface ViewController () <OLAssetsPickerControllerDelegate,
 #ifdef OL_KITE_AT_LEAST_IOS8
@@ -156,9 +177,19 @@ UINavigationControllerDelegate, OLKiteDelegate>
     vc.userEmail = @"";
     vc.userPhone = @"";
     vc.delegate = self;
-    vc.useDarkTheme = YES;
     [self presentViewController:vc animated:YES completion:NULL];
-
+    
+    //Register for push notifications
+    NSUInteger types = (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8) {
+        [[UIApplication sharedApplication] registerUserNotificationSettings:
+         [UIUserNotificationSettings settingsForTypes:types categories:nil]];
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+    }
+    //    else {
+    //        [[UIApplication sharedApplication] registerForRemoteNotificationTypes:types];
+    //    }
+    
 }
 
 - (IBAction)onButtonPrintRemotePhotos:(id)sender {
@@ -242,9 +273,9 @@ UINavigationControllerDelegate, OLKiteDelegate>
 #pragma mark - OLKiteDelete
 
 - (BOOL)kiteController:(OLKiteViewController *)controller isDefaultAssetsGroup:(ALAssetsGroup *)group {
-//    if ([[group valueForProperty:ALAssetsGroupPropertyName] isEqualToString:@"Instagram"]) {
-//        return YES;
-//    }
+    //    if ([[group valueForProperty:ALAssetsGroupPropertyName] isEqualToString:@"Instagram"]) {
+    //        return YES;
+    //    }
     return NO;
 }
 
