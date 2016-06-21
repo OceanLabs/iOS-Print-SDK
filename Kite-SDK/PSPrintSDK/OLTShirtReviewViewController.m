198e959d3a79b65110c814c4bb4f160666749bb3
Kostas Karayannis <kkarayannis@gmail.com>
2016-06-21 11:53:10 +0300

    commit work

---
+++ Kite-SDK/PSPrintSDK/OLTShirtReviewViewController.m
@@ -0,0 +1,24 @@
+//
+//  OLTShirtReviewViewController.m
+//  KitePrintSDK
+//
+//  Created by Konstadinos Karayannis on 17/06/16.
+//  Copyright © 2016 Kite.ly. All rights reserved.
+//
+
+#import "OLTShirtReviewViewController.h"
+
+@interface OLTShirtReviewViewController ()
+
+@end
+
+@implementation OLTShirtReviewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+
+@end
