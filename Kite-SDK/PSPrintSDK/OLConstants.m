91eeae62b7da19f0b7c005f364e6d9966aeb325f
Kostas Karayannis <kkarayannis@gmail.com>
2016-09-09 17:48:55 +0300

    Removed i and description from minimal white theme

--- Kite-SDK/PSPrintSDK/OLConstants.m
+++ Kite-SDK/PSPrintSDK/OLConstants.m
@@ -29,7 +29,7 @@
 
 #import "OLConstants.h"
 
-NSString *const kOLKiteSDKVersion = @"5.3.9";
+NSString *const kOLKiteSDKVersion = @"5.3.10";
 NSString *const kOLKiteSDKErrorDomain = @"co.oceanlabs.kOLKiteSDKErrorDomain";
 NSString *const kNotificationTemplateSyncComplete = @"co.oceanlabs.pssdk.notification.kNotificationSyncComplete";
 NSString *const kNotificationKeyTemplateSyncError = @"co.oceanlabs.pssdk.notification.kNotificationKeyTemplateSyncError";
