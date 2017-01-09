3f48cd867a4722459be8f8163b86c346380715c4
Kostas Karayannis <kkarayannis@gmail.com>
2017-01-09 15:43:18 +0100

    Fix edit animation for photobook cover

--- Kite-SDK/PSPrintSDK/OLPhotobookViewController.m
+++ Kite-SDK/PSPrintSDK/OLPhotobookViewController.m
@@ -922,7 +922,6 @@ static const CGFloat kBookEdgePadding = 38;
         [self.croppingPrintPhoto imageWithSize:[UIScreen mainScreen].bounds.size applyEdits:NO progress:NULL completion:^(UIImage *image, NSError *error){
             [cropVc setFullImage:image];
             cropVc.edits = self.croppingPrintPhoto.edits;
-            cropVc.modalPresentationStyle = [OLUserSession currentSession].kiteVc.modalPresentationStyle;
             [self presentViewController:cropVc animated:NO completion:NULL];
         }];
     }
