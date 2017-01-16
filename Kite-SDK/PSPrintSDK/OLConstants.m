2d730408e592cb236ed73a60d1898e2c78668710
Kostas Karayannis <kkarayannis@gmail.com>
2017-01-16 14:32:59 +0100

    Bump version

--- Kite-SDK/PSPrintSDK/OLConstants.m
+++ Kite-SDK/PSPrintSDK/OLConstants.m
@@ -29,7 +29,7 @@
 
 #import "OLConstants.h"
 
-NSString *const kOLKiteSDKVersion = @"6.1.15";
+NSString *const kOLKiteSDKVersion = @"6.1.16";
 NSString *const kOLKiteSDKErrorDomain = @"co.oceanlabs.kOLKiteSDKErrorDomain";
 NSString *const kNotificationTemplateSyncComplete = @"co.oceanlabs.pssdk.notification.kNotificationSyncComplete";
 NSString *const kNotificationKeyTemplateSyncError = @"co.oceanlabs.pssdk.notification.kNotificationKeyTemplateSyncError";
