5d5b5497060b790295c0aa33b2b037c1f6f1c3a2
Kostas Karayannis <kkarayannis@gmail.com>
2017-02-06 10:20:27 +0100

    Translation files are converted to and from csv

--- Kite-SDK/update-localization-files.py
+++ Kite-SDK/update-localization-files.py
@@ -1,9 +1,13 @@
 import sys
 import io
+import csv
 
 file = open(sys.argv[1], 'r')
 untranslated = open(sys.argv[3], 'w')
+untranslatedWriter = csv.writer(untranslated, delimiter=',', quotechar='"')
+
 result = open(sys.argv[4], 'w')
+
 
 for line in file:
     if (len(line) < 2 or line[0] == """/"""):
@@ -15,23 +19,21 @@ for line in file:
     found = False
     
     translated = open(sys.argv[2], 'r')
-    for translatedLine in translated:
-        if (len(translatedLine) < 2 or translatedLine[0] == """/"""):
+    translatedReader = csv.reader(translated, delimiter=',', quotechar='"')
+    for translatedLine in translatedReader:
+        if len(translatedLine) < 2:
             continue
-        translatedStrings = translatedLine.split(" = ")
-        if len(translatedStrings) < 2:
-            continue
-            
-        if strings[0] == translatedStrings[0]:
+           
+        if (strings[0])[1:-1] == translatedLine[0]:
             found = True
-            result.write(translatedLine)
+            result.write('"' + translatedLine[0] + "\" = \"" + translatedLine[1] + "\";\n")
             break
 
     if found == False:
-        untranslated.write(line)
+        untranslatedWriter.writerow([strings[0][1:-1],strings[1][1:-3]])
     
     translated.close()
         
+untranslated.close()
 file.close()
 result.close()
-untranslated.close()
