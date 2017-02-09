e1ae9802c6037f09aa542876d53202dfe3422435
Kostas Karayannis <kkarayannis@gmail.com>
2017-02-09 14:41:53 +0100

    Localization script now runs outside of Xcode

---
+++ Kite-SDK/update-localization-files.sh
@@ -0,0 +1,43 @@
+command -v iconv >/dev/null 2>&1 || { exit 0; }
+
+cd PSPrintSDK/OLKiteLocalizationResources.bundle/
+
+find ../ -name \*.m | xargs genstrings -o .
+rm Localizable.strings
+
+filename="KitePrintSDK.strings"
+translated="KitePrintSDK.translated.csv"
+converted="KitePrintSDK.converted.strings"
+untranslated="KitePrintSDK.untranslated.csv"
+result="KitePrintSDK.result.strings"
+
+iconv -f UTF-16 -t UTF-8 $filename > $converted
+if [ ! -f $converted ]; then
+cp $filename $converted
+fi
+
+sed -i -e 's/\%1\$/\%/g' $converted
+sed -i -e 's/\%2\$/\%/g' $converted
+sed -i -e 's/\%3\$/\%/g' $converted
+
+rm $filename
+
+for lang in `find . -name \*.lproj`; do
+
+cd $lang
+pwd
+
+touch $untranslated
+touch $result
+touch $translated
+
+python ../../../update-localization-files.py "`pwd`/../$converted" "`pwd`/$translated" "`pwd`/$untranslated" "`pwd`/$result"
+
+mv $result $filename
+
+cd ..
+
+done
+
+rm $converted
+rm $converted-e
