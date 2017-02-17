1a9275a8e2283b1a1385b308d3a36bca9cbd8e15
Kostas Karayannis <kkarayannis@gmail.com>
2017-02-17 14:25:30 +0100

    Increase cell size

--- Kite-SDK/PSPrintSDK/OLImageEditViewController.m
+++ Kite-SDK/PSPrintSDK/OLImageEditViewController.m
@@ -1660,7 +1660,7 @@ const NSInteger kOLEditTagCrop = 40;
         return CGSizeMake(120, self.editingTools.collectionView.frame.size.height);
     }
     
-    return CGSizeMake(self.editingTools.collectionView.frame.size.height * 1.6, self.editingTools.collectionView.frame.size.height);
+    return CGSizeMake(self.editingTools.collectionView.frame.size.height * 1.65, self.editingTools.collectionView.frame.size.height);
 }
 
 - (UIEdgeInsets)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout insetForSectionAtIndex:(NSInteger)section{
