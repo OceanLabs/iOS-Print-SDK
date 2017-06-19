670f2dbd0284d2d0a4498ccaa2eed89b2ad6a9f5
Kostas Karayannis <kkarayannis@gmail.com>
2017-06-19 13:58:05 +0200

    Added KiteUtils framework

---
+++ Kite-SDK/KiteUtils/KiteUtils.h
@@ -0,0 +1,20 @@
+//
+//  KiteUtils.h
+//  KiteUtils
+//
+//  Created by Konstadinos Karayannis on 19/06/2017.
+//  Copyright © 2017 Kite.ly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KiteUtils.
+FOUNDATION_EXPORT double KiteUtilsVersionNumber;
+
+//! Project version string for KiteUtils.
+FOUNDATION_EXPORT const unsigned char KiteUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KiteUtils/PublicHeader.h>
+
+#import <KiteUtils/UIColor+OLHexString.h>
+#import <KiteUtils/UIView+AutoLayoutHelper.h>
