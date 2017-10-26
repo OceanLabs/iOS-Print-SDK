e0958407611c446b5b5e66edfbfbda9059d10fed
Kostas Karayannis <kkarayannis@gmail.com>
2017-10-26 13:59:19 +0200

    Fixed not being able to add duplicate photos to a multiimage product

--- Kite-SDK/PSPrintSDK/OLImagePickerPhotosPageViewController.m
+++ Kite-SDK/PSPrintSDK/OLImagePickerPhotosPageViewController.m
@@ -616,6 +616,7 @@ CGFloat OLImagePickerMargin = 1.5;
         }
         else if (asset){ //Add photo
             asset = [asset copy];
+            asset.uuid = [[NSUUID UUID] UUIDString];
             NSUInteger index = [self.imagePicker.selectedAssets indexOfObject:[[OLPlaceholderAsset alloc] init]];
             if (index != NSNotFound){
                 [self.imagePicker.selectedAssets replaceObjectAtIndex:index withObject:asset];
